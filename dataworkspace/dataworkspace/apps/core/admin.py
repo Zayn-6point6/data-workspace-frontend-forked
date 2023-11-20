5c01c8379d004251cc09b3c06ac726ceb09f48ad
Nick Ross <nick.ir.ross@gmail.com>
2023-11-20 16:02:31 +0000

    fix: update csp directive for ckeditor admin pages

--- dataworkspace/dataworkspace/apps/core/admin.py
+++ dataworkspace/dataworkspace/apps/core/admin.py
@@ -58,11 +58,11 @@ class CSPRichTextEditorMixin:
     # which gives rich-text formatting but unfortunately uses inline scripts to
     # do so - and we don't have a clean way to either hash the inline script on-demand
     # or inject our request CSP nonce.
-    @csp_update(SCRIPT_SRC=["'unsafe-inline'", "'unsafe-hashes'"])
+    @csp_update(SCRIPT_SRC=["'unsafe-inline'", "'self'"])
     def add_view(self, request, form_url="", extra_context=None):
         return super().add_view(request, form_url, extra_context)
 
-    @csp_update(SCRIPT_SRC=["'unsafe-inline'", "'unsafe-hashes'"])
+    @csp_update(SCRIPT_SRC=["'unsafe-inline'", "'self'"])
     def change_view(self, request, object_id, form_url="", extra_context=None):
         return super().change_view(request, object_id, form_url, extra_context)
 
